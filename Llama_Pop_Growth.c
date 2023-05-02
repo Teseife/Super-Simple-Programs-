2ee973c5486d035b80e6027f924c3d987ca38666
Thomas Seife <92551215+Teseife@users.noreply.github.com>
2023-05-02 11:23:17 -0400

    Update Llama_Pop_Growth.c

--- Llama_Pop_Growth.c
+++ Llama_Pop_Growth.c
@@ -1,71 +1,84 @@
 #include <stdio.h>
 
-
 /* Author: Thomas Seife
  * Date: 3-16-2023
- * Description: This is simple program that calculates the Population growth of lamas. Uses simple formulas for
- *              finding lamas born and passed that year to appropriately find how many years it would take to reach
- *              a certain population.
+ * Description: This is a simple program that calculates the population growth of llamas. 
+ *              It uses simple formulas for finding the number of llamas born and passed away 
+ *              each year to determine how many years it would take to reach a certain population.
  */
 
 // prototypes:
-int startvalue(int start);
-int endvalue(int end, int start);
-int year_amount(int start, int end, int years);
+int get_start_population(void);
+int get_end_population(int start_population);
+int calculate_years_to_reach_population(int start_population, int end_population);
 
 int main()
 {
     // Variables and function callers
-    int start = 0;
-    int end = 0,years = 0;
-    start = startvalue(start);
-    end = endvalue(end,start);
-    years = year_amount(start,end,years);
+    int start_population = get_start_population();
+    int end_population = get_end_population(start_population);
+    int years_to_reach_population = calculate_years_to_reach_population(start_population, end_population);
+
     // Output:
-    printf("Years: %d", (int)years);
+    printf("It will take %d years to reach a population of %d llamas.\n", years_to_reach_population, end_population);
 
     return 0;
 }
-//**************************************************************************************
-int startvalue(int start)
+
+/**
+ * Prompts the user to enter the starting population of llamas.
+ *
+ * @return The starting population of llamas entered by the user.
+ */
+int get_start_population(void)
 {
+    int start_population;
     do
     {
-        printf("Start Size:");
-        scanf("%d",&start);
-    } while (start < 9);
-
-    if(start >= 9)
-        return start;
-    return start;
+        printf("Enter the starting population of llamas (must be at least 9): ");
+        scanf("%d", &start_population);
+    } while (start_population < 9);
+    
+    return start_population;
 }
-//**************************************************************************************
-int endvalue(int end, int start)
+
+/**
+ * Prompts the user to enter the ending population of llamas.
+ *
+ * @param start_population The starting population of llamas.
+ *
+ * @return The ending population of llamas entered by the user.
+ */
+int get_end_population(int start_population)
 {
-    do {
-        printf("End start:");
-        scanf("%d", &end);
-    } while (end < start);
-
-    if(end > start){
-        return end;
-    }// end of if (end > start)
-    return end;
-
-}// end of endvalue()
-
-//**************************************************************************************
-int year_amount(int start, int end, int years)
-{
-    int i = 0;
-    while(start < end)
+    int end_population;
+    do
     {
-        years = start + start/3 - start/4;
-        start = years;
-        i++;
-        if(start >= end)
-            return i;
-    }// end of while(start < end)
-    return i;
-}// end of year_amount()
-//**************************************************************************************
+        printf("Enter the ending population of llamas (must be greater than or equal to the starting population of %d): ", start_population);
+        scanf("%d", &end_population);
+    } while (end_population < start_population);
+
+    return end_population;
+}
+
+/**
+ * Calculates the number of years it will take to reach the ending population of llamas
+ * given the starting population and the rate of population growth.
+ *
+ * @param start_population The starting population of llamas.
+ * @param end_population The ending population of llamas.
+ *
+ * @return The number of years it will take to reach the ending population of llamas.
+ */
+int calculate_years_to_reach_population(int start_population, int end_population)
+{
+    int years_to_reach_population = 0;
+    while (start_population < end_population)
+    {
+        int llamas_born = start_population / 3;
+        int llamas_passed = start_population / 4;
+        start_population += llamas_born - llamas_passed;
+        years_to_reach_population++;
+    }
+    return years_to_reach_population;
+}
